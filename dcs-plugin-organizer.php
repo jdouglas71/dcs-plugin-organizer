97469b32d2d50a795d13a4422b3fd4d7a6c3c332
Jason Douglas <jdouglas71@gmail.com>
2015-02-17 10:56:58 -0800

    Added slimstat

--- dcs-plugin-organizer.php
+++ dcs-plugin-organizer.php
@@ -12,15 +12,26 @@ License: GPL
 
 function dcs_disable_landing_pages_plugin($plugins)
 {
+	//Disable the landing page plugin
     if(strpos($_SERVER['REQUEST_URI'], '/go/ppc-landing/') === FALSE AND strpos($_SERVER['REQUEST_URI'], '/wp-admin/') === FALSE) 
 	{
         $key = array_search( 'landing-pages/landing-pages.php' , $plugins );
         if ( false !== $key ) 
 		{
-			//error_log( "Unsetting the Landing Page plugin for page: ".$_SERVER['REQUEST_URI'], 3, plugin_dir_path(__FILE__)."/dcs-plugin-organizer.log" );
+			error_log( "Unsetting the Landing Page plugin for page: ".$_SERVER['REQUEST_URI'], 3, plugin_dir_path(__FILE__)."/dcs-plugin-organizer.log".PHP_EOL );
 			unset( $plugins[$key] );
 		}
     }
+
+	if(strpos($_SERVER['REQUEST_URI'], '/product/ripcord/') === TRUE AND strpos($_SERVER['REQUEST_URI'], '/wp-admin/') === FALSE) 
+	{
+		$key = array_search( 'wp-slimstat/wp-slimstat.php' , $plugins );
+		if ( false !== $key ) 
+		{
+			error_log( "Unsetting the SlimStat plugin for page: ".$_SERVER['REQUEST_URI'], 3, plugin_dir_path(__FILE__)."/dcs-plugin-organizer.log".PHP_EOL );
+			unset( $plugins[$key] );
+		}
+	}
 
 	return $plugins;
 }
