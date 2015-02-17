b608b5c39ab0e5b1425c353fa659d2c28e9afe01
Jason Douglas <jdouglas71@gmail.com>
2015-02-17 09:25:12 -0800

    First Commit

---
+++ dcs-plugin-organizer.php
@@ -0,0 +1,28 @@
+<?php
+/*
+Plugin Name: DCS Plugin Organizer
+Plugin URI: http://douglasconsulting.net/
+Description: Enables plugins for specific pages. Currently specific for RipCordTravelProtection and the Landing Pages plugin.
+Version: 0.5
+Author: Jason Douglas
+Author URI: http://douglasconsulting.net
+License: GPL
+*/
+
+
+function dcs_disable_landing_pages_plugin($plugins)
+{
+    if(strpos($_SERVER['REQUEST_URI'], '/go/ppc-landing/') === FALSE AND strpos($_SERVER['REQUEST_URI'], '/wp-admin/') === FALSE) 
+	{
+        $key = array_search( 'landing-pages/landing-pages.php' , $plugins );
+        if ( false !== $key ) 
+		{
+			error_log( "Unsetting the Landing Page plugin for page: ".$_SERVER['REQUEST_URI'], 3, plugin_dir_path(__FILE__)."/dcs-plugin-organizer.log" );
+			unset( $plugins[$key] );
+		}
+    }
+
+	return $plugins;
+}
+add_filter( 'option_active_plugins', 'dcs_disable_landing_pages_plugin' );
+
